ebe65f14936c50f556143e0ce78cf4898228a62f
Ilya <IlyaAbdullinWork@yandex.ru>
2021-10-26 00:38:11 +0500

    Added readme

--- js/render.js
+++ js/render.js
@@ -42,6 +42,6 @@ function renderTable(tableDataURL) {
 
 renderTable(
   localStorage.type === 'part'
-    ? 'https://jsonplaceholder.typicode.com/users'
-    : 'https://jsonplaceholder.typicode.com/users'
+    ? 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&adress={addressObject}&description={lorem|32}'
+    : 'http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&adress={addressObject}&description={lorem|32}'
 )
